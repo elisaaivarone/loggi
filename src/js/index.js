6c1f211ad6ef90c24c67f08a74197e0f56c42b16
janarf <janarf@hotmail.com>
2019-07-24 14:51:39 -0300

    ./src code breaking

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
-  console.log('oi')
   $('#enter').click(function (event) {
     event.preventDefault();
     window.location = "map.html";
   });
   $("#loggi-logo").click((e) => {
     e.preventDefault();
-    console.log('olar')
   })
 
   $("#select-region").change(function () {
