db6239d1cfb14d0246a8095046ddca6d9794c181
janarf <janarf@hotmail.com>
2019-07-24 10:51:22 -0300

    funcao setcenter ok, link com select nao funciona

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-
-$("#enter").click(function (event) {
+  console.log('oi')
+  $('#enter').click(function (event) {
     event.preventDefault();
     window.location = "map.html";
   });
+  $("#loggi-logo").click((e) => {
+    e.preventDefault();
+    console.log('olar')
+  })
 });
 
 
